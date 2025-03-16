7dbdd7526f87d47525fc4b008733aef06b98b93d
Sai Ganesh Chamarty <123051270+SaiChamarty@users.noreply.github.com>
2025-03-15 22:21:12 -0700

    Kruskal algorithm with Union-Find implemented

--- kruskal.py
+++ kruskal.py
@@ -16,3 +16,60 @@ graph = {
     'D': {'A': 6, 'B': 7, 'C': 4, 'E': 2},
     'E': {'A': 8, 'B': 9, 'C': 6, 'D': 2}
 }
+
+# Time Complexity: O(E log E)
+
+class UnionSet:
+    def __init__(self):
+        self.parent = self
+        self.rank = 0
+    
+    # find function that returns the parent of the UnionSet, which is the tree
+    def find(self):
+        if self.parent == self:
+            return self
+        else:
+            self.parent = self.parent.find()
+            return self.parent
+    
+    # add a vertex and an edge to this tree
+    def union(self, other: "UnionSet"):
+        rootX = self.find()
+        rootY = other.find()
+
+        if rootX != rootY:
+            if rootX.rank > rootY.rank:
+                rootY.parent = rootX
+            elif rootX.rank < rootY.rank:
+                rootX.parent = rootY
+            else:
+                rootY.parent = rootX
+                rootX.rank += 1
+
+# Initialize Union-Find for each vertex
+sets = {vertex: UnionSet() for vertex in graph}
+
+# sort the edges in the graph in ascending order in a list called edges:
+edges = []
+seen = set() # to avoid duplicate edges
+
+for u in graph:
+    for v, weight in graph[u].items():
+        if (v, u) not in seen: # ensure we don't add both (A, B) and (B, A)
+            edges.append((weight, sets[u], sets[v], u, v))
+            seen.add((u, v))
+
+# Sort edges by weight in ascending order
+edges.sort(key=lambda edge: edge[0])
+
+# Apply Kruskal's algorithm
+MST = [] # minimum spanning tree
+
+for weight, u, v, u_name, v_name in edges:
+    if u.find() != v.find(): # only if they are in different sets
+        MST.append((u_name, v_name, weight)) # Store the edge (A, B, eight)
+        u.union(v) # merge sets
+
+# Print Minimum Spanning Tree
+for edge in MST:
+    print(f"{edge[0]} - {edge[1]} -> {edge[2]}")
